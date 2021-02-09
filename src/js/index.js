787fcfd8281af831cc57a4ee5dfa0ef1219ab781
Daemonium97 <daemonium97@gmail.com>
2021-02-09 18:14:07 +0000

    counter

--- src/js/index.js
+++ src/js/index.js
@@ -11,5 +11,31 @@ import "../styles/index.scss";
 //import your own components
 import { Home } from "./component/home.js";
 
-//render your react application
-ReactDOM.render(<Home />, document.querySelector("#app"));
+const Counter = properties => {
+	let numbersArray = properties.number.split("");
+	const howManyCeros = 9 - numbersArray.length;
+
+	for (let i = 0; i < howManyCeros.length; i++) {
+		numbersArray.splice(0, 0, "0");
+	}
+
+	return (
+		<h1 className="d-flex bigCounter justify-content-center ">
+			<div className="Clock">
+				<i className="far fa-clock" />
+			</div>
+			{numbersArray.map((num, i) => (
+				<div key={i}>{num}</div>
+			))}
+		</h1>
+	);
+};
+let number = 0;
+
+setInterval(() => {
+	number = number + 1;
+	ReactDOM.render(
+		<Counter number={number.toString()} />,
+		document.querySelector("#app")
+	);
+}, 1000);
